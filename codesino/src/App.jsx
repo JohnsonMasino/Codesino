3095cfce7e616b158df7a1a49202b064775e2b90
Johnson Masino <johnsonmasino@gmail.com>
2025-03-27 17:37:10 +0100

    initial commit

---
+++ codesino/src/App.jsx
@@ -0,0 +1,14 @@
+import Reac from 'react';
+
+
+function App() {
+
+  return (
+    <div className='container bg-gray-400 mx-auto'>
+      <h1 className='text-gray-100'>Hello, welcome to Codesino!</h1>
+      <p className='text-gray-200'>Tailwind css is installed.</p>
+    </div>
+  );
+}
+
+export default App;
