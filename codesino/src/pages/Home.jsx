e47949fab9a5aee89190dde5bb20aadbb92c543a
Johnson Masino <johnsonmasino@gmail.com>
2025-03-30 01:08:29 +0100

    Added there header and navlinks

--- codesino/src/pages/Home.jsx
+++ codesino/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header.jsx';
 
 const Home = () => {
     return (
-        <div style={{ textAlign: 'center'}}>
+        <div style={{ textAlign: 'center'}} className='p-[1px]' >
             <Header />
             <h1>Welcome to Codesino</h1>
             <p>Tailwind css is installed fine!</p>
